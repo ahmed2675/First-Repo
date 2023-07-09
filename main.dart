991b190a1e5974e26917c2d764ab9986e5293ab9
ahmed <ahmed.2.6.7.5.3.9@gmail.com>
2023-07-09 14:59:15 +0500

    first Repooo

---
+++ main.dart
@@ -0,0 +1,90 @@
+import 'dart:io';
+void main(){
+//   Map world ={
+//     "pakistan":{
+//       "capitalsity" : "islamabad",
+//       "currency" : "PKR",
+//       "Languahe" : "Urdu",
+      
+//     },
+//     "india":{
+//       "capitalsity" : "delhi",
+//       "currency" : "rupee",
+//       "Languahe" : "hindi",
+      
+//     },
+//     "china":{
+//       "capitalsity" : "hhhh",
+//       "currency" : "hiiii",
+//       "Languahe" : "chini",
+      
+//     } 
+//   } ;
+// print(world["india"]['capitalsity']);
+//pre/post increment
+// var num1 = 0;
+// var num2 = ++num1;
+// var num3 = num2++;
+// print("num1 = $num1"); 
+// print("num2 = $num2"); 
+// print("num3 = $num3"); 
+
+// var num4=2;
+// var num5 =
+
+// var table = 5;
+// for (var i =1; i<11;i++){
+// print("$table * $i = ${table*i}");
+
+// }
+
+// for (int i =1; i<10;i++){
+//   print("Table Numbers ${i}");
+//   for(int j = 1; j <=10;j++){
+//     print("$i * $j = ${i*j}");
+//   }
+// }
+
+
+
+// List mylist =[1,2,3,4,5,6,7,8];
+// for(int i =0; i<mylist.length; i++  ){
+//   if(mylist[i] % 2 ==0){
+//     print("Even NUmbers IS ${mylist[i]}");
+//   }
+//   else(
+//     print("Odd numbers is ${mylist[i]} ")
+//   );
+// } 
+
+
+//   var email = stdin.readByteSync();
+//   var pass = stdin.readByteSync();
+// bool isloggin = false;
+// while(!isloggin){
+//   if(email == "email@gmail.com" && pass == "0987"){
+//     isloggin = true;
+//     print("login Succesfull");
+//   }
+//   else{
+//     print("Try Again");
+
+//   }
+
+// }
+List myList = [12,34,21,32,45,67]
+List Evennumbers = [];
+List Oddnumbers = [];
+for(var i in myList)  {
+if(i % 2 == 0) {
+  EvenNumbers.add(i);
+}
+else {
+  OddNumbers.add(i);
+}
+}
+
+
+
+}
+
